ce6655c638fc3cccade3843047d8dbc3fc0d5d1b
Zeeshan <113969161+Zeehaan@users.noreply.github.com>
2022-09-28 10:24:36 +0530

    Add files via upload

--- exception/CustomisedResponseEntityExceptionHandler.java
+++ exception/CustomisedResponseEntityExceptionHandler.java
@@ -1,44 +1,44 @@
-package com.practice.webservices.restfulwebservices.exception;
-
-
-import java.util.Date;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.practice.webservices.restfulwebservices.user.UserNotFoundException;
-
-@ControllerAdvice
-@RestController
-public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {	
-
-	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
-	ExceptionResponse exceptionresponse=new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	return new ResponseEntity<Object>(exceptionresponse,HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-	@Override
-	public ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(), ex.getMessage(), 
-				"Validation failed");
-		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
-	}
-	
-
-	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
-	ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_FOUND);
-	}
-}
- 
+//package com.practice.webservices.restfulwebservices.exception;
+//
+//
+//import java.util.Date;
+//
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//import com.practice.webservices.restfulwebservices.user.UserNotFoundException;
+//
+//@ControllerAdvice
+//@RestController
+//public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {	
+//
+//	@ExceptionHandler(Exception.class)
+//	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+//	ExceptionResponse exceptionresponse=new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+//	return new ResponseEntity<Object>(exceptionresponse,HttpStatus.INTERNAL_SERVER_ERROR);
+//	}
+//	
+//	@Override
+//	public ResponseEntity<Object> handleMethodArgumentNotValid(
+//			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//
+//		ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(), ex.getMessage(), 
+//				"Validation failed");
+//		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
+//	}
+//	
+//
+//	@ExceptionHandler(UserNotFoundException.class)
+//	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+//	ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+//	return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_FOUND);
+//	}
+//}
+// 
